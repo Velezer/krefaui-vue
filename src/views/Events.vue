67e7aa622695c557a748154f2c0f2e6140c43d20
Arief Syaifuddin <asvelezer@gmail.com>
2021-08-19 08:50:15 +0700

    getAttendance

--- src/views/Events.vue
+++ src/views/Events.vue
@@ -1,8 +1,39 @@
 <template>
-  <h1>Events</h1>
-  <div v-for="event in events" :key="event.id">
-    <router-link :to="{ name: 'Events_id', params:{id:event.id }} ">{{ event.judul }}</router-link>
+  <div class="container">
+    <h1>Daftar Events</h1>
+    <div class="col-sm-5">
+      <!-- <h3>Event : Peresmian KreFa</h3> -->
+    </div>
+    <div class="col-sm-5">
+      <h3>Hari : Senin,1 Juni 2021</h3>
+    </div>
+
+    <table style="width: 85%">
+      <tr>
+        <th style="padding-left: 10px; padding-right: 10px">No</th>
+        <th>Judul</th>
+        <th>Pembicara</th>
+        <th>Tempat</th>
+        <th>Tanggal</th>
+      </tr>
+
+      <tr v-for="event in events" :key="event.id">
+        <td>{{ event.id }}</td>
+        <td>
+          <router-link :to="{ name: 'Events_id', params: { id: event.id } }">
+            {{ event.judul }}
+          </router-link>
+        </td>
+        <td>{{ event.pembicara }}</td>
+        <td>{{ event.tempat }}</td>
+        <td>{{ event.tanggal }}</td>
+      </tr>
+    </table>
   </div>
+
+  <br>
+  <br>
+  <br>
 </template>
 
 
@@ -10,11 +41,11 @@
 const axios = require("axios").default;
 
 export default {
-  props: ["id"],
+  // props: ["id"],
   data() {
     return {
       events: [],
-      urlEvents: "http://" + location.hostname + ":8080",
+      urlEvents: `http://${location.hostname}:8080`,
     };
   },
   created() {
--- src/views/EventsID.vue
+++ src/views/EventsID.vue
@@ -1,10 +1,7 @@
 <template>
-
   <div class="container">
-    <h1>Daftar <br />Presensi Kehadiran</h1>
-    <div class="col-sm-5">
-      <h3>Event : Peresmian KreFa</h3>
-    </div>
+    <h1>Daftar Peserta <br />{{ eventName }}</h1>
+    <div class="col-sm-5"></div>
     <div class="col-sm-5">
       <h3>Hari : Senin,1 Juni 2021</h3>
     </div>
@@ -13,43 +10,78 @@
       <tr>
         <th style="padding-left: 10px; padding-right: 10px">No</th>
         <th>Nama</th>
-        <th>No Tiket</th>
-        <th>Tanggal</th>
-        <th>Status</th>
+        <th>Hadir Pada</th>
       </tr>
 
-      <tr>
-        <td>1</td>
-        <td style="text-align: left; padding-right: 220px">Lorem Ipsum</td>
-        <td>#786788389349</td>
-        <td>29 Juni 2021, 16.00</td>
-        <td>Datang</td>
-      </tr>
-      <tr>
-        <td>2</td>
-        <td style="text-align: left">Lorem Ipsum</td>
-        <td>#84759868949894</td>
-        <td>29 Juni 2021, 16.00</td>
-        <td>Datang</td>
-      </tr>
-      <tr>
-        <td>3</td>
-        <td style="text-align: left">Lorem Ipsum</td>
-        <td>#589789894589</td>
-        <td>29 Juni 2021, 16.00</td>
-        <td>Belum Datang</td>
+      <tr v-for="(person, i) in attendance" :key="person.id">
+        <td>{{ i + 1 }}</td>
+        <td>
+          <router-link :to="{ name: 'People_id', params: { id: person.id } }">
+            {{ person.nama }}
+          </router-link>
+        </td>
+        <td>{{ person.hadir_pada }}</td>
       </tr>
     </table>
   </div>
+
+  <br />
+  <br />
+  <br />
 </template>
 
 
 <script>
+const axios = require("axios").default;
+
 export default {
   props: ["id"],
+  data() {
+    return {
+      attendance: [],
+      baseAPIURL: `http://${location.hostname}:8080`,
+      eventName: "",
+    };
+  },
+  created() {
+    axios.defaults.headers.common[
+      "Authorization"
+    ] = `Bearer ${localStorage.getItem("access-token")}`;
+    this.getAttendanceById(this.id);
+  },
+  methods: {
+    getAttendanceById(id) {
+      axios
+        .get(`${this.baseAPIURL}/api/attendance/events/${id}`)
+        .then((res) => {
+          let data = res.data;
+          this.attendance = data.data;
+          this.eventName = this.attendance[0]["judul"];
+        })
+        .catch((err) => {
+          if (err.response.status == 401) {
+            this.$router.push({ name: "Login" });
+          }
+        });
+    },
+    deleteAttendance(id) {
+      axios
+        .delete(this.baseAPIURL + "/api/attendace/" + id)
+        .then((res) => {
+          let data = res.data;
+          if (data.status == "success") {
+            alert(data);
+          }
+        })
+        .catch((err) => {
+          if (err.response.status == 401) {
+            this.$router.push({ name: "Login" });
+          }
+        });
+    },
+  },
 };
 </script>
-
 
 
 <style>
@@ -69,8 +101,7 @@ export default {
   padding-bottom: 50px;
   padding: auto;
 }
-.col-sm-5{
-padding-left: 180px;
+.col-sm-5 {
+  padding-left: 180px;
 }
-
 </style>
