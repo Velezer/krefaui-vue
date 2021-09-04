70e10b6f7f43a148f1b02be5f8079cd7d3748d7f
Arief Syaifuddin <48375364+Velezer@users.noreply.github.com>
2021-09-04 21:19:10 +0700

    KeyPage

---
+++ src/views/KeyPage.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="Link2">
+    <router-link :to="{ name: 'Home' }"> Home </router-link>
+    <router-link :to="{ name: 'Events' }"> Events </router-link>
+    <router-link :to="{ name: 'Events_id' }"> EventsID </router-link>
+    <router-link :to="{ name: 'Login' }"> LoginAdmin </router-link>
+    <router-link :to="{ name: 'NewEvent' }"> NewEvent </router-link>
+    <router-link :to="{ name: 'People' }"> People </router-link>
+    <router-link :to="{ name: 'People_id' }"> PeopleID </router-link>
+    <router-link :to="{ name: 'Presensi' }"> Presensi </router-link>
+    <router-link :to="{ name: 'PresensiTanpa' }"> PresensiTanpa </router-link>
+    <router-link :to="{ name: 'Events_id' }"> EventsID </router-link>
+    <router-link :to="{ name: 'Register' }"> Register </router-link>
+    <router-link :to="{ name: 'People_id_update' }"> RegisterUpdate </router-link>
+    <router-link :to="{ name: 'Sync' }"> Sync </router-link>
+    <router-link to="/notfound404"> NotFound </router-link>
+  </div>
+</template>>
+
