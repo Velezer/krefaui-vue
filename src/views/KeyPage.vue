11c3fd056eefb59269142d8579cc5adac5e39807
Arief Syaifuddin <48375364+Velezer@users.noreply.github.com>
2021-09-04 21:24:01 +0700

    Update KeyPage.vue

--- src/views/KeyPage.vue
+++ src/views/KeyPage.vue
@@ -2,16 +2,24 @@
   <div class="Link2">
     <router-link :to="{ name: 'Home' }"> Home </router-link>
     <router-link :to="{ name: 'Events' }"> Events </router-link>
-    <router-link :to="{ name: 'Events_id' }"> EventsID </router-link>
+    <router-link :to="{ name: 'Events_id', params: { id: 1 } }">
+      EventsID
+    </router-link>
     <router-link :to="{ name: 'Login' }"> LoginAdmin </router-link>
     <router-link :to="{ name: 'NewEvent' }"> NewEvent </router-link>
     <router-link :to="{ name: 'People' }"> People </router-link>
-    <router-link :to="{ name: 'People_id' }"> PeopleID </router-link>
+    <router-link :to="{ name: 'People_id', params: { id: 1 } }">
+      PeopleID
+    </router-link>
     <router-link :to="{ name: 'Presensi' }"> Presensi </router-link>
     <router-link :to="{ name: 'PresensiTanpa' }"> PresensiTanpa </router-link>
-    <router-link :to="{ name: 'Events_id' }"> EventsID </router-link>
+    <router-link :to="{ name: 'Events_id', params: { id: 1 } }">
+      EventsID
+    </router-link>
     <router-link :to="{ name: 'Register' }"> Register </router-link>
-    <router-link :to="{ name: 'People_id_update' }"> RegisterUpdate </router-link>
+    <router-link :to="{ name: 'People_id_update', params: { id: 1 } }">
+      RegisterUpdate
+    </router-link>
     <router-link :to="{ name: 'Sync' }"> Sync </router-link>
     <router-link to="/notfound404"> NotFound </router-link>
   </div>
