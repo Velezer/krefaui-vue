b1f5093d5669b06c89caac0f7173352a906662ab
Arief Syaifuddin <asvelezer@gmail.com>
2021-09-23 09:03:04 +0700

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -1,8 +1,8 @@
 // let dataUrl = `http://${location.hostname}:8080`
+// let faceUrl = `http://${location.hostname}`
 // let faceUrl = `http://${location.hostname}:8000`
 let dataUrl = `https://krefa-data-api.herokuapp.com`
-let faceUrl = `http://${location.hostname}`
-// let faceUrl = `https://goface-api-echo-velezer.herokuapp.com/`
+let faceUrl = `https://goface-api-echo.herokuapp.com`
 
 let config = {
     baseImgUrl: dataUrl,
